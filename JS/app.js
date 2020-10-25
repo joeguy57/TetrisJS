c80e376dd5efcf32c5ac663ff59cbe2f9b164657
Joseph <56702275+joeguy57@users.noreply.github.com>
2020-10-24 18:39:42 -0600

    changing css files

--- JS/app.js
+++ JS/app.js
@@ -257,3 +257,10 @@ function gameOver() {
         clearInterval(timerId);
     }
 }
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
